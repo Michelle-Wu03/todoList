2fa0075c979c06b9d6fcb0c7780fb6c15532cea5
Michelle-Wu03 <73873509+Michelle-Wu03@users.noreply.github.com>
2024-01-19 09:34:05 +0800

    Add files via upload

---
+++ todoList.vue
@@ -0,0 +1,29 @@
+<template id="app" v-cloak>
+  <div>
+    <input v-model="newTodo" placeholder="新增待辦" />
+    <button @click="addTodo">Add Todo</button>
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">{{ todo.text }}
+        <button @click="removeTodo(index)">Remove</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import { mapState, mapActions } from 'vuex';
+
+  export default {
+    data() {
+        return {
+        newTodo: ''
+        };
+    },
+    computed: {
+        ...mapState(['todos']),
+    },
+    methods: {
+        ...mapActions(['addTodo', 'removeTodo']),
+    }
+  };
+</script>
